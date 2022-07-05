7300543da9320e3617ae94086f377e5778347da9
SebastianRaveArroyave <sebastianrave2927@gmail.com>
2022-07-05 17:33:40 -0500

    Ejercicio_Tema_4_Terminado

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args)
     {
         /*Usando un if, crear una condición que compare si la variable numeroIf es positivo, negativo, o 0.*/
-        System.out.println("Condicional if");
+        System.out.println("Condicional if!!");
         int NumeroIf = -5;
 
         if (NumeroIf > 0){
@@ -14,7 +14,7 @@ public class Main {
         }
 
         //Crea un bucle While, este bucle tendrá que tener como condición que la variable numeroWhile sea inferior a 3
-        System.out.println("Bucle while");
+        System.out.println("Bucle while!!");
         int NumeroWhile = 0;
         while ( NumeroWhile < 3 ){
             System.out.println(NumeroWhile);
@@ -23,16 +23,46 @@ public class Main {
         }
 
         //Para el bucle Do While, deberás crear la misma estructura que en el While
-        System.out.println("Bucle do while");
-        NumeroWhile = 0;
+        System.out.println("Bucle do while!!");
+        int NumeroDoWhile = 0;
         do{
-            System.out.println(NumeroWhile);
-            NumeroWhile++;
+            System.out.println(NumeroDoWhile);
+            NumeroDoWhile++;
 
-        }while ( NumeroWhile ==0 );
+        }while ( NumeroDoWhile == 0 );
+
 
 
         //Para el bucle For, crea una variable numeroFor,
+        System.out.println("Bucle For!!");
+        for ( int NumeroFor = 0; NumeroFor <=3;NumeroFor++)
+        {
+            System.out.println(NumeroFor);
+
+        }
+
+
+        //Por último, para el Switch, deberás crear la variable estacion
+        System.out.println("Switch Case!!");
+        var Estacion = "Primavera";
+
+        switch (Estacion){
+            case "Verano":
+                System.out.println("Es Verano");
+                break;
+            case "Invierno":
+                System.out.println("Es Invierno");
+                break;
+            case "Otoño":
+                System.out.println("Es Otoño");
+                break;
+            case "Primavera":
+                System.out.println("Es Primavera");
+                break;
+            default:
+                System.out.println("Default");
+
+        }
 
     }
 }
